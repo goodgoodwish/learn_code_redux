4fee1593182a18268b7041ecbefd742fcaef0291
Charlie Zhu <goodgoodwish@gmail.com>
2017-07-26 18:56:03 -0700

    5.Redux middleware

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,38 @@
-import {combineReducers, createStore} from "redux"
+import {applyMiddleware, combineReducers, createStore} from "redux"
 
+const logger = (store) => (next) => (action) => {
+	console.log('Log: Action fired, ', action);
+	//action.payload = 1;
+	next(action);
+}
+
+const error = (store) => (next) => (action) => {
+	try {
+		next(action);
+	} catch(e) {
+		console.log('Error : ', e.message);
+	}
+}
+
+const middleware = applyMiddleware(logger, error);
+
+const singleReducer = function (initState = 0, action) {
+	switch (action.type) {
+		case 'INC':
+			initState += action.payload;
+			break;
+		case 'DEC':
+			initState -= action.payload;
+			break;
+		case 'E':
+			throw new Error('Throw a new error!!!!');
+			break;
+		default:
+			// statements_def
+			break; 
+	}
+	return initState;
+}
 const userReducer = function (state = {}, action) {
 	switch (action.type) {
 		case 'CHANGE_NAME':
@@ -18,7 +51,7 @@ const userReducer = function (state = {}, action) {
 const tweetReducer = function (state = [], action) {
 	switch (action.type) {
 		case 'ADD_TWEET':
-			state = [...state, action.payload];
+			state = [...state, {id: Date.now(), text:action.payload}];
 			break;
 		default:
 			// statements_def
@@ -28,11 +61,14 @@ const tweetReducer = function (state = [], action) {
 }
 
 const reducers = combineReducers({
-	userA: userReducer,
-	tweetsB: tweetReducer,
+	useRS: userReducer,
+	tweetsRS: tweetReducer,
 })
 
-const store = createStore(reducers);
+//const store = createStore(reducers);
+//const store = createStore(singleReducer, 1, middleware);
+const store = createStore(reducers, {}, middleware);
+
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
@@ -41,7 +77,11 @@ store.subscribe(() => {
 store.dispatch({type: "CHANGE_NAME", payload: ''})
 store.dispatch({type: "CHANGE_NAME", payload: 'Juliet'})
 store.dispatch({type: "CHANGE_AGE", payload: 18})
+//
+//store.dispatch({type: "ADD_TWEET", payload: ''})
+//store.dispatch({type: "ADD_TWEET", payload: 'Good wish'})
+//store.dispatch({type: "ADD_TWEET", payload: 'Mindful listening'})
 
-store.dispatch({type: "ADD_TWEET", payload: ''})
-store.dispatch({type: "ADD_TWEET", payload: 'Good wish'})
-store.dispatch({type: "ADD_TWEET", payload: 'Mindful listening'})
+//store.dispatch({type: "INC", payload: 1})
+//store.dispatch({type: "DEC", payload: 1})
+//store.dispatch({type: "E", payload: 1})
